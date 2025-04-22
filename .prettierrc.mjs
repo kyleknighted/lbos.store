cfeba529d48806c8bc887fa6002c5c9d9de19092
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2025-04-21 21:09:47 -0500

    fixup prettier config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,10 +1,14 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
-  overrides: [
-    {
-      files: "*.astro",
-      options: { parser: "astro" },
-    },
-  ],
+	printWidth: 100,
+	useTabs: true,
+	plugins: ["prettier-plugin-astro"],
+	overrides: [
+		{
+			files: ["**/*.astro"],
+			options: {
+				parser: "astro",
+			},
+		},
+	],
 };
