347fa23df4b3e8a09c4d77de4c0ac20a86ced3b0
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2025-04-21 21:43:21 -0500

    migrating to astro architecture

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,14 +1,18 @@
 /** @type {import("prettier").Config} */
 export default {
-	printWidth: 100,
-	useTabs: true,
-	plugins: ["prettier-plugin-astro"],
-	overrides: [
-		{
-			files: ["**/*.astro"],
-			options: {
-				parser: "astro",
-			},
-		},
-	],
-};
+  printWidth: 100,
+  useTabs: false,
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: ['**/*.astro'],
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+}
